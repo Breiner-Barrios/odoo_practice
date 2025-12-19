95cce443d081c6cc49ca80f3cb5e5947dddef349
Breiner Barrios <breinerbarriosc@gmail.com>
2025-12-19 10:33:39 -0500

    setup adoo

---
+++ addons/mi_modulo_test/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Mi Modulo Test',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Módulo de prueba para mi nuevo entorno Docker',
+    'depends': ['base'],
+    'data': [
+        # Aquí irán tus archivos XML de vistas más adelante
+    ],
+    'installable': True,
+    'application': True,
+}
